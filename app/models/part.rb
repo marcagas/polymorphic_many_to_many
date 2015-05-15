b3d94911c0961ad9b51469a25905159b34c8ed02
Marc Agas <marc.agas@stellarloyalty.com>
2015-05-15 08:03:18 +0800

    Done testing STI.

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,4 @@
 class Part < ActiveRecord::Base
-  has_many :messages, through: :message_parts, as: :messageable
-  has_many :message_parts, as: :messageable
+  has_many :message_parts
+  has_many :messages, through: :message_parts, source: :messageable, source_type: 'Message'
 end
